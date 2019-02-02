68dece3100b16b38d4e4aec5336a760cbe4da4cf
Filip H.F. "FiXato" Slagter <fixato@gmail.com>
2019-02-02 19:09:57 +0100

    Added cross-platform support for sed. Should work on macOS now (though it will prefer GNU sed (installed through homebrew) over BSD sed.

--- store_comments_frame.sh
+++ store_comments_frame.sh
@@ -4,13 +4,23 @@ DATADIR="./data/output"
 BASE_URL="https://apis.google.com/u/0/_/widget/render/comments?first_party_property=BLOGGER&query="
 mkdir -p $DATADIR
 stdin=$(cat)
+
+
+function gnused() {
+  if hash gsed 2>/dev/null; then
+      gsed -E "$@"
+  else
+      sed -E "$@"
+  fi
+}
+
 while read -r post_url
 do
-  domain=$(echo "$post_url"| sed -r 's/https?:\/\/([^/]+)\/.+/\1/gi' -| sed 's/[^-a-z0-9_.]/-/ig' -)
+  domain=$(echo "$post_url" | gnused 's/https?:\/\/([^/]+)\/.+/\1/g' | gnused 's/[^-a-zA-Z0-9_.]/-/g')
   #echo "$domain"
   url="$BASE_URL$post_url"
   #echo "$post_url"
-  filename=$(echo "$post_url" | sed 's/[^-a-z0-9_.]/-/ig' -)
+  filename=$(echo "$post_url" | gnused 's/[^-a-zA-Z0-9_.]/-/g')
   #echo $filename
   mkdir -p "$DATADIR/$domain"
   path="$DATADIR/$domain/$filename"
