e5f54df4443c57b7cca3b512b8b3fc49c7365db0
Filip H.F. "FiXato" Slagter <fixato@gmail.com>
2019-03-15 07:38:25 +0100

    Added support for automatically finding the numeric ID for +CustomURLHandle G+ profile URLs/IDs

--- bin/people_api_data_for_gplus_profile.sh
+++ bin/people_api_data_for_gplus_profile.sh
@@ -33,14 +33,44 @@ function handle_failure() {
   fi
 }
 
-json_output_file="$(user_profile_file "$user_id" "day")" || exit 255
-profile_api_url="$(api_url "gplus" "people" "get" "$user_id")" || exit 255
+# Get the numeric UserID from the profile page if the given $user_id is in the +CustomHandle format.
+if [ "${user_id:0:1}" == "+" ]; then
+  user_id_custom_to_numeric_map_filepath="$(ensure_path "${caller_path}/../data/gplus/custom_to_numeric_user_id_mappings" "${user_id:1}.txt")"
+  if [ -f "$user_id_custom_to_numeric_map_filepath" ]; then
+    numeric_user_id="$(cat "$user_id_custom_to_numeric_map_filepath")"
+    debug "Retrieved numeric user_id '$numeric_user_id' for '$user_id' from '$user_id_custom_to_numeric_map_filepath'."
+  else
+    archived_profile_page="$("$caller_path/archive_url.sh" "https://plus.google.com/${user_id}")"
+    exit_code="$?"
+    if (( $exit_code > 0 )); then
+      echo "Error while archiving G+ profile page for ${user_id}. Exited with error code $exit_code" 1>&2
+      exit $exit_code
+    fi
+    
+    if hash pup 2>/dev/null; then
+      numeric_user_id="$(cat "$archived_profile_page" | pup 'link[itemprop] attr{href}')"
+      numeric_user_id="${numeric_user_id#"https://plus.google.com/"}"
+    else
+      numeric_user_id="$(cat "$archived_profile_page" | gnugrep -oP '<link itemprop="url" href="\K([^"]+)')"
+    fi
+  fi
+else
+  numeric_user_id="$user_id"
+fi
+
+json_output_file="$(user_profile_file "$numeric_user_id" "day")" || exit 255
+profile_api_url="$(api_url "gplus" "people" "get" "$numeric_user_id")" || exit 255
 
 #filename=$(cache_remote_document_to_file "$profile_api_url" "$json_output_file") && cat "$filename" || handle_failure
 filename=$(cache_remote_document_to_file "$profile_api_url" "$json_output_file")
 if (( $? >= 1 )); then
   # FIXME: find out why I can't exit with an error code, as it seems to make xargs running in parallel mode stop working when it encounters an error on one of its processes.
   handle_failure "$2" # && exit 255
+elif [ "$user_id" != "$numeric_user_id" ];then
+  json_output_file="$(user_profile_file "$user_id" "day")"
+  if [ ! -f "$json_output_file" ]; then
+    ln "$filename" "$json_output_file" 1>&2
+  fi
 fi
 
 echo "$filename"
