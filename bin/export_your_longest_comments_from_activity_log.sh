e09d49eb2adff5aa06fef4788a8656db54f4431c
Filip H.F. "FiXato" Slagter <fixato@gmail.com>
2019-03-15 18:50:41 +0100

    Added script to expect your ActivityLog comments.
    This will get all your comments (.items[] .primaryText) from your ActivityLog and store them in data/output/html/exported_comments/$commentLength-$commentTimestampMs-$first50charactersFromComment.txt.
    Using GNU sort you can sort these on their natural filename to get them sorted by comment length (as I was too lazy to zero-pad the filenames through jq): find data/output/html/exported_comments/ | gsort -V

---
+++ bin/export_your_longest_comments_from_activity_log.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+# encoding: utf-8
+activity_log_filepath="$1"
+if [ "$activity_log_filepath" == "" ]; then
+  printf "Please specify the path to activity log file. e.g.:\n$0 \"extracted/Takeout/Google+ Stream/ActivityLog/Comments.json\"\n" 1>&2
+  exit 255
+fi
+
+data_dir=${2:-"data/output/html/exported_comments"}
+mkdir -p "$data_dir"
+while IFS= read -r -d '' comment; do
+  newline=$'
+'
+  comment="$(printf '%s' "$comment" | tr -d '\0' )"
+  filename="${comment%% *}"
+  filename="${filename#*$newline}"
+  echo "Exporting to ${data_dir}/${filename}" 1>&2
+  comment_text="${comment#* }"
+  # echo "Filename: $filename"
+  # echo "Comment: $comment_text"
+  printf '%s' "$comment_text" > "${data_dir}/${filename}"
+done < <(jq -r '.items[] | (.primaryText|length|tostring) + "-" + .timestampMs + "-" + (.primaryText|gsub("[^a-zA-Z0-9]+"; "_")[0:50]|gsub("_+$"; "")) + ".txt " + (.primaryText|gsub("  "; "\n")) + "\u0000"' "$activity_log_filepath")
