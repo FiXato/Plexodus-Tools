6408eca7d564ef6f487baa74313a42b99c86a193
Filip H.F. "FiXato" Slagter <fixato@gmail.com>
2019-03-21 15:25:05 +0100

    get_all_public_post_urls.sh: Exclude empty lines and sort unique the resulting files
    and use variable for finding all the result files in the output direct in get_all_profile_urls.sh

--- bin/get_all_public_post_urls.sh
+++ bin/get_all_public_post_urls.sh
@@ -20,19 +20,19 @@ output_post_urls_from_activitylog_plusones_on_comments="$(ensure_path "$output_d
 output_post_urls_from_activitylog_plusones_on_posts="$(ensure_path "$output_dir" "from_activitylog_plusones_on_posts.txt")"
 output_all_unique_public_post_urls="$(ensure_path "$output_dir" "all_unique_public_post_urls.txt")"
 
-find "$extracted_takeout_directory/Google+ Stream/Posts" -type f -print0 -iname '*.json' | $xargs_cmd -0 jq -r 'select(.postAcl .isPublic == true)| [.]|map(.url)|[.]|flatten|add|[.]|unique|join("\n")' | tee "$output_public_post_urls_from_takeout_stream_posts"
+find "$extracted_takeout_directory/Google+ Stream/Posts" -type f -print0 -iname '*.json' | $xargs_cmd -0 jq -r 'select(.postAcl .isPublic == true)| [.]|map(.url)|[.]|flatten|add|[.]|unique|join("\n")' | tee "$output_public_post_urls_from_takeout_stream_posts" && sort -uo "$output_public_post_urls_from_takeout_stream_posts" "$output_public_post_urls_from_takeout_stream_posts"
 
 # For some reason I can't use -print0 with -path as it then seems to ignore the -path argument
 # I unfortunately also had issues with macOS's BSD version of xargs when not using null-byte delimited filenames, so I had to resort to GNU xargs when available.
-find "$extracted_takeout_directory/Google+ Communities" -type f -path '*/Posts/*.json' | $xargs_cmd -I@@ jq -r 'select(.postAcl .isPublic == true)| [.]|map(.url)|[.]|flatten|add|[.]|unique|join("\n")' "@@" | tee "$output_public_post_urls_from_takeout_community_posts"
+find "$extracted_takeout_directory/Google+ Communities" -type f -path '*/Posts/*.json' | $xargs_cmd -I@@ jq -r 'select(.postAcl .isPublic == true)| [.]|map(.url)|[.]|flatten|add|[.]|unique|join("\n")' "@@" | tee "$output_public_post_urls_from_takeout_community_posts" && sort -uo "$output_public_post_urls_from_takeout_community_posts" "$output_public_post_urls_from_takeout_community_posts"
 
-jq -r '[.items[]|select(.visibility == "PUBLIC")]|map(.commentCreatedItem .postPermalink)|join("\n")' "$extracted_takeout_directory/Google+ Stream/ActivityLog/Comments.json" | tee "$output_post_urls_from_activitylog_comments"
+jq -r '[.items[]|select(.visibility == "PUBLIC")]|map(.commentCreatedItem .postPermalink)|join("\n")' "$extracted_takeout_directory/Google+ Stream/ActivityLog/Comments.json" | tee "$output_post_urls_from_activitylog_comments" && sort -uo "$output_post_urls_from_activitylog_comments" "$output_post_urls_from_activitylog_comments"
 
-jq -r '[.items[]|select(.visibility == "PUBLIC")]|map(.commentPlusOneAddedItem .postPermalink)|join("\n")' "$extracted_takeout_directory/Google+ Stream/ActivityLog/+1s on comments.json" | tee "$output_post_urls_from_activitylog_plusones_on_comments"
+jq -r '[.items[]|select(.visibility == "PUBLIC")]|map(.commentPlusOneAddedItem .postPermalink)|join("\n")' "$extracted_takeout_directory/Google+ Stream/ActivityLog/+1s on comments.json" | tee "$output_post_urls_from_activitylog_plusones_on_comments" && sort -uo "$output_post_urls_from_activitylog_plusones_on_comments" "$output_post_urls_from_activitylog_plusones_on_comments"
 
-jq -r '[.items[]|select(.visibility == "PUBLIC")]|map(.postPlusOneAddedItem .postPermalink)|join("\n")' "$extracted_takeout_directory/Google+ Stream/ActivityLog/+1s on posts.json" | tee "$output_post_urls_from_activitylog_plusones_on_posts"
+jq -r '[.items[]|select(.visibility == "PUBLIC")]|map(.postPlusOneAddedItem .postPermalink)|join("\n")' "$extracted_takeout_directory/Google+ Stream/ActivityLog/+1s on posts.json" | tee "$output_post_urls_from_activitylog_plusones_on_posts" && sort -uo "$output_post_urls_from_activitylog_plusones_on_posts" "$output_post_urls_from_activitylog_plusones_on_posts"
 
-jq -r '[.items[]|select(.visibility == "PUBLIC")]|map(.pollVoteAddedItem .postPermalink)|join("\n")' "$extracted_takeout_directory/Google+ Stream/ActivityLog/Poll Votes.json" | tee "$output_post_urls_from_activitylog_poll_votes"
+jq -r '[.items[]|select(.visibility == "PUBLIC")]|map(.pollVoteAddedItem .postPermalink)|join("\n")' "$extracted_takeout_directory/Google+ Stream/ActivityLog/Poll Votes.json" | tee "$output_post_urls_from_activitylog_poll_votes" && sort -uo "$output_post_urls_from_activitylog_poll_votes" "$output_post_urls_from_activitylog_poll_votes"
 
 # echo "$output_public_post_urls_from_takeout_stream_posts"
 # echo "$output_public_post_urls_from_takeout_community_posts"
@@ -41,4 +41,4 @@ jq -r '[.items[]|select(.visibility == "PUBLIC")]|map(.pollVoteAddedItem .postPe
 # echo "$output_post_urls_from_activitylog_plusones_on_posts"
 # echo "$output_post_urls_from_activitylog_poll_votes"
 
-sort -u "$output_public_post_urls_from_takeout_stream_posts" "$output_public_post_urls_from_takeout_community_posts" "$output_post_urls_from_activitylog_comments" "$output_post_urls_from_activitylog_poll_votes" "$output_post_urls_from_activitylog_plusones_on_comments" "$output_post_urls_from_activitylog_plusones_on_posts" > "$output_all_unique_public_post_urls"
+sort -u $(find "$output_dir" -iname 'from_*.txt' -type f | xargs) | exclude_empty_lines > "$output_all_unique_public_post_urls"
