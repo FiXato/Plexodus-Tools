921fa77f0cc45b096a86e837e999eae58a707255
Filip H.F. "FiXato" Slagter <fixato@gmail.com>
2019-03-25 04:16:35 +0100

    cd into the cloned repo

--- bin/plexodus-tools.sh
+++ bin/plexodus-tools.sh
@@ -25,7 +25,7 @@ setup() {
     git status > /dev/null
     if [ "$?" != "0" ]; then
       echo "Replacing this script with the full git repository"
-      git clone https://github.com/FiXato/Plexodus-Tools && rm plexodus-tools.sh && ln -s bin/plexodus-tools.sh ./
+      git clone https://github.com/FiXato/Plexodus-Tools && rm plexodus-tools.sh && cd Plexodus-Tools && ln -s bin/plexodus-tools.sh ./
     fi
   fi
 }
