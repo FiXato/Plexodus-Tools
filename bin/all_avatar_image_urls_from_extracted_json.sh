3caf67f775030414c775ce09cfa86f49226ca834
Filip H.F. "FiXato" Slagter <fixato@gmail.com>
2019-04-01 01:59:40 +0200

    Added support for setting a find maxdepth through PLEXODUS_FIND_MAX_DEPTH
    So far it's only supported yet for bin/all_avatar_image_urls_from_extracted_json.sh

--- bin/all_avatar_image_urls_from_extracted_json.sh
+++ bin/all_avatar_image_urls_from_extracted_json.sh
@@ -10,5 +10,7 @@ sed_rules+=('s/\/s[0-9]{2,}-[a-zA-Z]\//\//g') # strip /s64-c/ and similar (re-)s
 sed_rules+=($'s/^\/\//https:\/\//') # Replace protocol-agnostic URLs with https:// URLs.
 search_path="${1:-$PLEXODUS_EXTRACTED_TAKEOUT_PARENT_PATH}/"
 avatars_output_path="$(output_path "all_avatar_image_urls-without_size${2:-""}")"
-find "$search_path" -iname '*.json' -exec "$(gnugrep_cmdstring)" -oP '"avatarImageUrl":\s{0,}"\K([^" ]{1,})' "{}" \; | gnused -E "$(printf '%s' "${sed_rules[@]/%/;}")" | sort -u | tee "$avatars_output_path"
-echo "${TP_MAGENTA}$avatars_output_path${TP_RESET}" 1>&2
+(( "$PLEXODUS_FIND_MAX_DEPTH" >= 0 )) && find_depth=" -maxdepth $PLEXODUS_FIND_MAX_DEPTH"
+debug "${FG_MAGENTA}Searching for JSON files in '$search_path' at a max depth of '$PLEXODUS_FIND_MAX_DEPTH' and saving output to '$avatars_output_path'${TP_RESET}"
+find "$search_path" -iname '*.json'$find_depth -exec "$(gnugrep_cmdstring)" -oP '"avatarImageUrl":\s{0,}"\K([^" ]{1,})' "{}" \; | gnused -E "$(printf '%s' "${sed_rules[@]/%/;}")" | sort -u | tee "$avatars_output_path"
+echo "${FG_MAGENTA}$avatars_output_path${TP_RESET}" 1>&2
