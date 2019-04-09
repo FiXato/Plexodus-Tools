d76793fea856c75f53d31775253b45a92c3b72c9
Filip H.F. "FiXato" Slagter <fixato@gmail.com>
2019-04-09 07:11:57 +0200

    More detailed handling of JSON files with missing media files.
    Logging the JSON file in the downloaded file via xattr now
    Also preparing for tracking downloaded remote files when there's no localFilePath.
    Added counters to get an idea of progress.
    Should also handle the last item in each file now, when when there's no trailing null-byte.

--- bin/download_missing_media_files.sh
+++ bin/download_missing_media_files.sh
@@ -41,42 +41,54 @@ link_file() {
   fi
 }
 
-while IFS= read -r -d '' line; do
-  remote_url="${line%%$'\u001F'*}"
-  local_filepath="${line//*$'\u001F'}"
-  if [ "$local_filepath" != "" ]; then
-    if [ -f "$local_filepath" ]; then
-      echo "$local_filepath"
-    else
-      debug "${FG_MAGENTA}Local file '$local_filepath' does not exist"
-      downloaded_fps="$("$PT_PATH/bin/retrieve_googleusercontent_url.sh" "$remote_url")"
-      exit_code="$?"
-      if (( $exit_code == 0 )); then
-        downloaded_fp="$(realpath "${downloaded_fps//*$'\n'}")"
-        debug "downloaded filepath: '$downloaded_fp'"
-        debug "target filepath: '$local_filepath'"
-        [ -f "$local_filepath" ] && debug "file '$local_filepath' exists"
-        link_file "$downloaded_fp" "$local_filepath"
-        exit_code="$?"
-        debug "exit_code: $exit_code"
-        if (( $exit_code == 0 ));then
-          debug "linking succeeded"
-          realpath --no-symlinks "$local_filepath"
-        else
-          debug "linking exited with $exit_code"
-          echo "$downloaded_fp"
-        fi
+declare -a items=()
+total_results="$(find "${takeout_posts_paths[@]/%/\/Takeout\/Google+ Stream\/Posts\/}" -iname '*.json' -type f -print0 2> /dev/null | "$GREP_CMD" -cz '^')"
+while IFS= read -r -d '' json_source_fp || [ -n "$json_source_fp" -a "$json_source_fp" != "" ]; do
+  debug "JSON [$((counter+=1))/$total_results]: $json_source_fp"
+  total_media_items="$(jq -r '.album .media | length' "$json_source_fp")"
+  media_counter=0
+  while IFS= read -r -d '' line || [ -n "$line" -a "${line//$'\n'/}" != "" ]; do
+    items=()
+    split "$line" items $'\u001F' > /dev/null 2>&1
+    remote_url="${items[0]}"
+    local_filepath="${items[1]}"
+    debug "[$((media_counter+=1))/$total_media_items]${FG_MAGENTA}Remote: '$remote_url'
+${FG_MAGENTA}Local: '$local_filepath'${TP_RESET}"
+
+    if [ "$local_filepath" != "" ]; then
+      if [ -f "$local_filepath" ]; then
+        debug "\$local_filepath '$local_filepath' already exists"
+        echo "$local_filepath"
       else
-        error "Error while retrieving $remote_url"
-        echo "$remote_url"
+        debug "${FG_MAGENTA}Local file '$local_filepath' does not exist"
+        downloaded_fps="$("$PT_PATH/bin/retrieve_googleusercontent_url.sh" "$remote_url")"
+        exit_code="$?"
+        if (( $exit_code == 0 )); then
+          downloaded_fp="$(realpath "${downloaded_fps//*$'\n'}")"
+          setxattr "json_source" "$json_source_fp" "$downloaded_fp" 1>&2
+
+          [ -f "$local_filepath" ] && debug "file '$local_filepath' exists"
+          link_file "$downloaded_fp" "$local_filepath"
+          exit_code="$?"
+          if (( $exit_code == 0 ));then
+            debug "linking succeeded: $downloaded_fp -> $local_filepath"
+            realpath --no-symlinks "$local_filepath"
+          else
+            debug "linking exited with $exit_code"
+            echo "$downloaded_fp"
+          fi
+        else
+          error "[\$?=$exit_code] Error while retrieving $remote_url"
+          echo "$remote_url"
+        fi
       fi
+      continue
     fi
-    continue
-  fi
-  #TODO: I also want to store a reference to the JSON file in the xattr, so I need to redo the while loop; add another while loop for each JSON file.
-  #TODO: also download this, and perhaps add the reference to the local file path to the source JSON
-  echo "$remote_url"
-  # printf 'remote: "%s"\nlocal: %s\n\n' "$remote_url" "$local_filepath"
-done < <(find "${takeout_posts_paths[@]/%/\/Takeout\/Google+ Stream\/Posts\/}" -iname '*.json' -type f -exec jq -L "${PT_PATH}" -r --arg source_file "{}" 'include "plexodus-tools"; get_all_resource_urls_and_local_filepaths | map(.[0] as $remote| .[1] as $local| [$remote, (if $local|length > 0 then ($source_file|gsub("/[^/]+$"; "") + $local) else "" end)]|join("\u001F")) | join("\u0000")' '{}' \;)
+    #TODO: also download this, and perhaps add the reference to the local file path to the source JSON
+    debug "No local filepath specified. Returning \$remote_url"
+    echo "$remote_url"
+    # printf 'remote: "%s"\nlocal: %s\n\n' "$remote_url" "$local_filepath"
+  done < <(jq -L "${PT_PATH}" -r --arg source_file "$json_source_fp" 'include "plexodus-tools"; get_all_resource_urls_and_local_filepaths | map(.[0] as $remote| .[1] as $local| [$remote, (if $local|length > 0 then ($source_file|gsub("/[^/]+$"; "") + $local) else "" end)]|join("\u001F")) | join("\u0000")' "$json_source_fp")
+done < <(find "${takeout_posts_paths[@]/%/\/Takeout\/Google+ Stream\/Posts\/}" -iname '*.json' -type f -print0)
 
 #gnugrep -v '^$' | gnused 's/^\/\//https:\/\//' | sort -u | "${XARGS_CMD}" -I@@ "${PT_PATH}/bin/retrieve_googleusercontent_url.sh" "@@"
